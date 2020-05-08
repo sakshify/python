90810af3f4de6096f700036feb5a2dd4d951ecd1
Sakshi P <55315183+sakshify@users.noreply.github.com>
2020-05-08 13:01:41 +0530

    update 12.A.py
    Please accept this pull request.

--- college-projects/12.A.py
+++ college-projects/12.A.py
@@ -5,3 +5,4 @@ sorted_d = dict(sorted(d.items(), key=operator.itemgetter(1)))
 print('Dictionary in ascending order by value : ',sorted_d)
 sorted_d = dict(sorted(d.items(), key=operator.itemgetter(1),reverse=True))
 print('Dictionary in descending order by value : ',sorted_d)
+###project ends here.
